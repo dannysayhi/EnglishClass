04372d397f159c23ac4de1b3f0410d49a327e61b
DESKTOP-2LEACOS\user <wxes9040106@gmail.com>
2019-05-05 17:26:10 +0800

    add push notification for rollcall

--- Server/EnglishCalssManager/EnglishCalssManager/Utility/Database/DatabaseManager.cs
+++ Server/EnglishCalssManager/EnglishCalssManager/Utility/Database/DatabaseManager.cs
@@ -16,9 +16,9 @@ namespace EnglishClassManager.Utility.Database
         public static DatabaseCoreRollcall _databaseCoreRollcall;
         public static void Initialize()
         {
-            //_databaseCore = new DatabaseCore("DESKTOP-2LEACOS\\SQLEXPRESS", "sa", "EnglishClassDBtest", "");//後面要輸入自己的資料庫密碼
-            //_databaseTable = new DatabaseTable();
-            //_databaseCoreRollcall = new DatabaseCoreRollcall("DESKTOP-2LEACOS\\SQLEXPRESS", "sa", "EnglishClassDBtestRollcall", "");//後面要輸入自己的資料庫密碼
+            _databaseCore = new DatabaseCore("67-0A60507-H1\\SQLEXPRESS", "sa", "EnglishClassDBtest", "b22303409");//後面要輸入自己的資料庫密碼
+            _databaseTable = new DatabaseTable();
+            _databaseCoreRollcall = new DatabaseCoreRollcall("67-0A60507-H1\\SQLEXPRESS", "sa", "EnglishClassDBtestRollcall", "b22303409");//後面要輸入自己的資料庫密碼
 
             //_databaseCore = new DatabaseCore("I22-3000000371", "sa", "EnglishClassDBtest", "");
             //_databaseTable = new DatabaseTable();
@@ -27,9 +27,9 @@ namespace EnglishClassManager.Utility.Database
             // Public database
             //Data Source=ouchunhsien.ddns.net\SQL,1433;Initial Catalog=EnglishClassDBtest;User ID=sa;Password=***********
 
-            _databaseCore = new DatabaseCore("ouchunhsien.ddns.net\\SQL,1433", "sa", "EnglishClassDBtest", "0426322358");
-            _databaseTable = new DatabaseTable();
-            _databaseCoreRollcall = new DatabaseCoreRollcall("ouchunhsien.ddns.net\\SQL,1433", "sa", "EnglishClassDBtestRollcall", "0426322358");
+            //_databaseCore = new DatabaseCore("ouchunhsien.ddns.net\\SQL,1433", "sa", "EnglishClassDBtest", "0426322358");
+            //_databaseTable = new DatabaseTable();
+            //_databaseCoreRollcall = new DatabaseCoreRollcall("ouchunhsien.ddns.net\\SQL,1433", "sa", "EnglishClassDBtestRollcall", "0426322358");
 
         }
 
